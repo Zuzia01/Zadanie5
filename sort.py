b9403c4efde71d29d46f53638ab59a160df895b1
Zuzanna Baran <81303008+Zuzia01@users.noreply.github.com>
2021-10-19 20:44:05 +0200

    Add files via upload

--- sort.py
+++ sort.py
@@ -1,42 +1,93 @@
-# <Zuzanna> <Baran>, <406267>
-from typing import Tuple, List
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 27 13:43:52 2018
 
-def quicksort(I: List[int]):
-    Ip = I.copy()
-    return quicksortdo(Ip)
+@author: XTreme.ws
+"""
 
-def quicksortdo(Ip: List[int], start: int = None, stop: int = None):
-    if start == None and stop == None:
-        start = 0
-        stop = len(Ip) - 1
-    i = start
-    j = stop
-    pivot: int = Ip[int((start + stop)/2)] if len(Ip) % 2 == 1 else Ip[int(((start + stop + 1)/2))]
-    while i < j :
-        while Ip[i] < pivot:
+from typing import List
+from timeit import timeit
+import random
+ 
+ 
+def quicksort(I: List[int], start_inx: int, stop_inx: int):
+    """Posortuj zakres tablicy metodą quicksort.
+ 
+    :param I: tablica do posortowania
+    :param start_inx: indeks początku zakresu do posortowania (włącznie)
+    :param stop_inx: indeks końca zakresu do posortowania (włącznie)
+    """
+    i = start_inx
+    j = stop_inx
+    # Jako element środkowy wybierz element leżący na środku zakresu.
+    # (Operator `a // b` zwraca część całkowitą z dzielenia a przez b.)
+    pivot_inx = (stop_inx + start_inx) // 2
+ 
+    while i < j:
+        while I[i] < I[pivot_inx]:
             i += 1
-        while Ip[j] > pivot:
-            j = j - 1
-        if i <= j :
-            Ip[i], Ip[j] = Ip[j], Ip[i] 
-            i = i + 1
-            j = j - 1
-            
-    if start < j:
-        quicksortdo(Ip, start, j)
-    if i < stop:
-        quicksortdo(Ip, i, stop)
-    return Ip
-
-def bubblesort(I: List[int]) -> Tuple[List[int], int]:
-    nb: int = 0
-    Ip = list(I)
-    n = len(Ip)
-    while n > 1:
-        i = 1
-        for i  in range(1,n):
-            if Ip[i - 1] > Ip[i]:
-                Ip[i - 1], Ip[i] =  Ip[i], Ip[i - 1]
-            nb += 1
-        n = n - 1
-    return (Ip, nb)
+        while I[j] > I[pivot_inx]:
+            j -= 1
+ 
+        if i <= j:
+            I[i], I[j] = I[j], I[i]
+            i += 1
+            j -= 1
+ 
+    if start_inx < j:
+        quicksort(I, start_inx, j)
+    if i < stop_inx:
+        quicksort(I, i, stop_inx)
+    return I
+ 
+def bubble_sort(I: List[int]) -> None:
+    """Posortuj tablicę metodą bąbelkową.
+ 
+    :param I: tablica do posortowania
+    """
+    n = len(I)
+    # Flaga określająca, czy podczas przeglądania listy dokonaliśmy zamiany.
+    was_swap = True
+ 
+    while (n > 1) and was_swap:
+        # Na początku każdego cyklu należy "wyzerować" flagę zamiany.
+        was_swap = False
+        for i in range(1, n):
+            if I[i-1] > I[i]:
+                I[i-1], I[i] = I[i], I[i-1]
+                was_swap = True
+        n -= 1
+ 
+print(quicksort(random.sample(range(100), 100), 0, 100 - 1))
+N_ELEMENTS = 9
+test_lists = {
+    'sorted_list': list(range(N_ELEMENTS)),
+    'reversed_list': list(range(N_ELEMENTS))[::-1],
+    'unsorted_list': random.sample(range(N_ELEMENTS), N_ELEMENTS),
+    'uniform_list': [1] * N_ELEMENTS
+}
+ 
+N_REPETITIONS = 5
+print('\n== Average execution times for N={n} repetitions ==\n'.format(n=N_REPETITIONS))
+for test_list_id in test_lists:
+    for algorithm_id in ['quicksort', 'bubble_sort']:
+        # Dokonaj wstępnej alokacji w celu zwiększenia szybkości działania.
+        timings = [None] * N_REPETITIONS
+        for repetition_inx in range(N_REPETITIONS):
+            # Mierz czas wykonania funkcji na kopii (!) listy testowej -
+            # w przeciwnym razie tylko pierwszy pomiar będzie się odbywał dla
+            # listy z elementami w oryginalnej kolejności, a następne - już
+            # dla posortowanej listy!
+            test_list_copy = test_lists[test_list_id].copy()
+ 
+            if algorithm_id == 'quicksort':
+                timed_command = '{func}(test_list_copy, 0, N_ELEMENTS - 1)'.format(func=algorithm_id)
+            else:
+                timed_command = '{func}(test_list_copy)'.format(func=algorithm_id)
+            exec_time = timeit(timed_command, number=1, globals=globals())
+            timings[repetition_inx] = exec_time
+        print('{test_list_id:>15} ({algorithm_id:>13}) :  {avg_timing:.6f}'.format(
+            test_list_id=test_list_id, algorithm_id=algorithm_id,
+            avg_timing=sum(timings) / len(timings)
+        ))
+    print()
